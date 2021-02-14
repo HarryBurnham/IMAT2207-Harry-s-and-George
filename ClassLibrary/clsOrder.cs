ff846d9709d9651519a9da43c4eb521252c09e7b
badnys <harryb2000@live.co.uk>
2021-02-14 00:34:11 +0000

    Added testing for order class properties

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary
+{
+    public class clsOrder
+    {
+        public int OrderId { get; set; }
+        public string OrderDate { get; set; }
+        public int Quantity { get; set; }
+        public int Cost { get; set; }
+        public string OrderType { get; set; }
+        public bool Dispatched { get; set; }
+        public int CustomerId { get; set; }
+
+    }
+}
--- Testing2/tstOrder.cs
+++ Testing2/tstOrder.cs
@@ -17,10 +17,64 @@ namespace Testing2
         [TestMethod]
         public void OrderIdPropertyOK()
         {
+           clsOrder AnOrder = new clsOrder();
+           int testData = 1;
+           AnOrder.OrderId = testData;
+           Assert.AreEqual(AnOrder.OrderId,testData);
+        }
+
+        [TestMethod]
+        public void OrderDatePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string testData = "test";
+            AnOrder.OrderDate = testData;
+            Assert.AreEqual(AnOrder.OrderDate, testData);
+        }
+
+        [TestMethod]
+        public void QuantityPropertyOK()
+        {
             clsOrder AnOrder = new clsOrder();
             int testData = 1;
-            AnOrder.OrderId = testData;
-            Assert.AreEqual(aQuantity.active,testData);
+            AnOrder.Quantity = testData;
+            Assert.AreEqual(AnOrder.Quantity, testData);
+        }
+
+        [TestMethod]
+        public void CostPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int testData = 1;
+            AnOrder.Cost = testData;
+            Assert.AreEqual(AnOrder.Cost, testData);
+        }
+
+        [TestMethod]
+        public void OrderTypePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string testData = "test";
+            AnOrder.OrderType = testData;
+            Assert.AreEqual(AnOrder.OrderType, testData);
+        }
+
+        [TestMethod]
+        public void DispatchedPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            bool testData = true;
+            AnOrder.Dispatched = testData;
+            Assert.AreEqual(AnOrder.Dispatched, testData);
+        }
+
+        [TestMethod]
+        public void CustomerIdPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int testData = 1;
+            AnOrder.CustomerId = testData;
+            Assert.AreEqual(AnOrder.CustomerId, testData);
         }
     }
 }
